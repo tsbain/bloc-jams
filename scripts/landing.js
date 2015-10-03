9aa2d2a8ce78370bf44c7846ba2999638bb85496
Tim Bain <timsbain@gmail.com>
2015-10-03 13:38:20 -0700

    assignment 23 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,38 +1,19 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";   
+};
+
 var animatePoints = function(points) {
-
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-     
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
   
-  //Automatically animate the points on a tall screen where scrolling can't trigger the animation
-  
+  //Automatically animate the points on a tall screen where scrolling can't trigger the animation  
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
   }
