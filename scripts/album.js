4e78d6c1b09c4fe9b11a90f9d42cbb13975388fd
Tim Bain <timsbain@gmail.com>
2015-10-07 17:49:29 -0700

    checkpoint 26 progress

--- scripts/album.js
+++ scripts/album.js
@@ -52,7 +52,7 @@ var albumJourney = {
 var createSongRow = function (songNumber, songName, songLength) {
 	var template = 
 		  '<tr class="album-view-song-item">'
-		+ '	<td class="song-item-number">' + songNumber + '</td>'
+		+ ' <td class="song-item-number data-song-number="' + songNumber + '">' + songNumber + '</td>'
 		+ '	<td class="song-item-title">' + songName + '</td>'
 		+ '	<td class="song-item-duration">' + songLength + '</td>'
 		+ '</tr>';
@@ -85,8 +85,33 @@ var setCurrentAlbum = function(album) {
 																						 
 };
 
-window.onload = function() {			
+// Elements we'll be adding listeners to
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+window.onload = function() {
+	
 	setCurrentAlbum(albumPicasso);
+		
+	songListContainer.addEventListener('mouseover', function(event) {
+		//Only target individual song rows during event delegation
+		if (event.target.parentElement.className === 'album-view-song-item') {
+			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+		}
+	});
+	
+	// This code not working? Not sure where it goes
+	for (i = 0; i < songRows.length; i++) {
+		songRows[i].addEventListener('mouseleave', function(event) {
+			this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+		});
+	}
+	
+	// End 
+	
 	
 	var albums = [albumPicasso, albumMarconi, albumJourney];
 	var index = 1;
@@ -99,4 +124,4 @@ window.onload = function() {
 			index = 0;
 		}
 	});	
-};			
+};
